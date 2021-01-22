c933c18eb3c0cbcdbe6f6af9fe4fe4da1cbf2f8a
Apsaravi Gamage <apsaravigamage@gmail.com>
2021-01-22 05:47:43 +0530

    Add data

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news/views/category_news.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,8 +9,40 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Flutter"),
+            Text("News",style:TextStyle(
+              color: Colors.blue
+            ))
+          ],
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: ,
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+
+  final imageUrl, categoryNews;
+  CategoryTile({this.imageUrl, this.categoryNews});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      
+      child: Stack(
+        children: <Widget>[
+          Image.network(imageUrl, width: 120,height: 80,)
+        ],
+      )
     );
   }
 }

c814b8a7f558095f498a4168a03bd8f5bbaf2c4b
Apsaravi Gamage <apsaravigamage@gmail.com>
2021-01-31 16:53:29 +0530

    connect with web

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter_news/helper/data.dart';
 import 'package:flutter_news/helper/news.dart';
 import 'package:flutter_news/models/article_model.dart';
 import 'package:flutter_news/models/category_model.dart';
+import 'package:flutter_news/views/article_view.dart';
 
 
 class Home extends StatefulWidget {
@@ -88,6 +89,7 @@ class _HomeState extends State<Home> {
                        imageUrl: articles[index].urlToImage,
                        title: articles[index].title,
                        desc: articles[index].description,
+                       url: articles[index].url,
                      );
                   }),
               )
@@ -137,31 +139,40 @@ class CategoryTile extends StatelessWidget {
 
 class BlogTile extends StatelessWidget {
 
-  final String imageUrl, title,desc;
-  BlogTile({@required this.imageUrl,@required this.title,@required this.desc});
+  final String imageUrl, title,desc,url;
+  BlogTile({@required this.imageUrl,@required this.title,@required this.desc,
+  @required this.url});
 
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom:16),
-      child: Column(
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.network(imageUrl),
-          ),
-          SizedBox(height:8),
-          Text(title,style: TextStyle(
-            fontSize:17,
-            color: Colors.black87,
-            fontWeight: FontWeight.w600,
-          ),),
-          SizedBox(height:8),
-          Text(desc, style: TextStyle(
-            color:Colors.black26
-          ),),
-        ]
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) => ArticleView(
+            blogUrl: url,
+          ) ));
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom:16),
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(imageUrl),
+            ),
+            SizedBox(height:8),
+            Text(title,style: TextStyle(
+              fontSize:17,
+              color: Colors.black87,
+              fontWeight: FontWeight.w600,
+            ),),
+            SizedBox(height:8),
+            Text(desc, style: TextStyle(
+              color:Colors.black26
+            ),),
+          ]
+        ),
       ),
     );
   }
